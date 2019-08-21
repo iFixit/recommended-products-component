ca90eee833b9f8369c883f230a1e78047b89ee11
Taylor Tobin <19tt94@gmail.com>
2019-08-20 17:15:09 -0700

    added total to react state

--- src/RecommendedProductsComponent.js
+++ src/RecommendedProductsComponent.js
@@ -19,6 +19,10 @@ const blue = '#1875f1';
 const RecommendedProducts = styled.section`
     padding: 20px 0;
     max-width: 1024px;
+
+    ${bp1} {
+        padding: 20px;
+    }
 `;
 
 const Container = styled.section`
@@ -75,9 +79,28 @@ const Grid = styled.div`
     }
 `;
 
-const Image = styled.img`
-    width: calc(33.33% - 10px);
-    padding: 10px 5px;
+const Image = styled.div`
+    position: relative;
+    width: calc(33.33% - 20px);
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        width: 100%;
+    }
+`;
+
+const Plus = styled.span`
+    position: absolute;
+    right: -4%;
+    height: -50%;
+
+    &:before {
+        content: '+';
+        color: ${gray};
+    }
 `;
 
 const Details = styled.section`
@@ -114,37 +137,73 @@ const Submit = styled.button`
 `;
 
 class RecommendedProductsComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: products.map((product) => {
+                return {
+                    "sku": product.sku,
+                    "price": product.price,
+                    "selected": product.selected
+                }
+            })
+        }
+    }
+
     render() {
         return (
             <RecommendedProducts className="recommended-products">
                 <Header>{header}</Header>
                 <Container>
                     <Grid>
-                        {products.map((product, key)=> (
-                            <Image key={key} src={product.image} alt={product.title}></Image>
-                        ))}
+                        {products.map((product, key)=> {
+                            if(key < products.length - 1) {
+                                return <Image key={key}><img src={product.image} alt={product.title} /><Plus></Plus></Image>
+                            }
+                            return <Image key={key}><img src={product.image} alt={product.title} /></Image>
+                        })}
                     </Grid>
                     <Details>
                         {products.map((product, key)=> {
-                            if (product.selected) {
-                                return <Product key={key}><Selected>This Product</Selected>{product.name}<Price>${product.price}</Price></Product>
+                            if (product.initial_product) {
+                                return <Product key={key}><Selected>This Item</Selected>{product.name}<Price>${product.price}</Price></Product>
                             }
-                            return <Product key={key}><input type="checkbox" onChange={this.getSelection} defaultChecked />{product.name}<Price>${product.price}</Price></Product>;
+                            return <Product key={key}><input type="checkbox" onChange={(e) => this.getSelection(product.sku, e)} defaultChecked />{product.name}<Price>${product.price}</Price></Product>;
                         })}
-                        <Price>${this.total()}</Price><Submit onClick={this.addToCart}>Add To Cart</Submit>
+                        <Price>${this.getTotal()}</Price><Submit onClick={this.addToCart}>Add To Cart</Submit>
                     </Details>
                 </Container>
             </RecommendedProducts>
         );
     }
 
-    total() {
-        return products.map(a => a.price).reduce((a, b) => parseFloat(a) + parseFloat(b), 0)
+    getTotal() {
+        return this.state.selected.map(a => (a.selected) ? a.price : 0).reduce((a, b) => parseFloat(a) + parseFloat(b), 0)
     }
 
-    getSelection(e) {
+    getSelection(key, e) {
         // handle checkbox change event
-        console.log(e);
+        if(e.target.checked) {
+            this.setState({
+                selected: this.state.selected.map((item) => {
+                    return {
+                        "sku": item.sku,
+                        "price": item.price,
+                        "selected": (item.sku === key) ? item.selected = true : item.selected
+                    }
+                })
+            })
+        } else {
+            this.setState({
+                selected: this.state.selected.map((item) => {
+                    return {
+                        "sku": item.sku,
+                        "price": item.price,
+                        "selected": (item.sku === key) ? item.selected = false : item.selected
+                    }
+                })
+            })
+        }
     }
 
     addToCart() {
