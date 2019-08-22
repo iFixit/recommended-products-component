b7e10ce571679d765e8028a0ae6b10d6ab4d9b68
David Rans <dave.g.rans@gmail.com>
2019-08-22 15:49:18 -0700

    initial commit

---
+++ src/RecommendedProductsComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RecommendedProductsComponent.css';
+
+function RecommendedProductsComponent() {
+  return (
+    <div className="recommended-products">
+
+    </div>
+  );
+}
+
+export default RecommendedProductsComponent;
